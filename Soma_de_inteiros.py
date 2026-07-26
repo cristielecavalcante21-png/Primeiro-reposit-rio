c93517c023e8913b7bcd4a0c0ececaec292e2e87
cristielecavalcante21-png <cristielecavalcante21@gmail.com>
2026-07-25 21:48:58 -0300

    Exemplo de uso do git status e git push com soma de inteiros

---
+++ Soma_de_inteiros.py
@@ -0,0 +1,12 @@
+'''
+Exercicio de Hoje: usar git status e git push
+
+Exemplo em Python: soma de inteiros
+'''
+print('----Bem-vindo ao programa Soma de inteiros!----')
+Numero_1 = int(input('Digite seu primeiro numero: '))
+Numero_2 = int(input('Digite seu segundo numero: '))
+
+soma = (Numero_1 + Numero_2)
+
+print(f'A soma: {soma}')
